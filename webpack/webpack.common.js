1bd1d68ef35f05d6afb8d66143fc827db8909217
Boris Butorin <butorinb.g.personal@gmail.com>
2024-09-12 02:03:52 +0300

    homepage_changed

--- webpack/webpack.common.js
+++ webpack/webpack.common.js
@@ -13,7 +13,7 @@ module.exports = {
 		filename: production
 			? 'static/scripts/[name].[contenthash].js'
 			: 'static/scripts/[name].js', // имя нашего бандла
-		publicPath: '/',
+		publicPath: '/blog-customizer/',
 	},
 	//Нужно помочь вебпаку научится работать с jsx и tsx файлами для этого используют ts loader
 	module: {
